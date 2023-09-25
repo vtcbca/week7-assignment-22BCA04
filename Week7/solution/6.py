eb92876f27abede5c6c3685580838785f0f81b62
Vishal Agrawal <122864330+22BCA04@users.noreply.github.com>
2023-09-25 10:53:33 +0530

    Update 6.py

--- Week7/solution/6.py
+++ Week7/solution/6.py
@@ -2,11 +2,11 @@
 import pandas as pd
 file="c://sqlite3//product.csv"
 df=pd.read_csv(file)
+
 #to know the length of the dataframe
 len(df)
 
-df.loc[2.5]=[3.4,'Mobile',4500,6000,10000,9500,8500,8000]
-df.loc[2.6]=[3.5,'Head-phone',6500,7200,3600,4500,9000,6000]
-
-df=df.sort_index(),reset_index()
+df.loc[2.5]=[15,'Music Player',5000,5000,5000,5000,5000,5000,30000]
+df.loc[2.6]=[16,'Sound System',6000,6000,6000,6000,6000,6000,36000]
+df=df.sort_index().reset_index(drop=True)
 print(df)
